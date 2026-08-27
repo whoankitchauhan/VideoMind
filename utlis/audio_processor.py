bd7e58aad54b93f5fd31eca6757721cc9129d8b3
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-28 00:39:28 +0530

    Refactor requirements.txt and audio_processor.py for clarity and consistency

--- utlis/audio_processor.py
+++ utlis/audio_processor.py
@@ -1,26 +1,37 @@
 import os
+import yt_dlp
+
 DOWNLOAD_DIR = os.path.join(os.getcwd(), "downloads")
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
-def download_youtube_audio(url: str)-> str:
-    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")   
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': output_path,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }
-        ],
-            "quiet": True
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info).replace('.webm', '.mp3').replace('.m4a', '.mp3')
 
+def download_youtube_audio(url: str) -> str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }],
+        "quiet": True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+
+        filename = (
+            filename
+            .replace(".webm", ".mp3")
+            .replace(".m4a", ".mp3")
+        )
 
     return filename
 
-download_youtube_audio("https://www.youtube.com/watch?v=7HSSR1n8dgc")
 
+print(download_youtube_audio(
+    "https://www.youtube.com/watch?v=7HSSR1n8dgc"
+))
